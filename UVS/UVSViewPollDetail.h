698a65471df5ed2998d4863da613844a85190b26
Umbruner <umbruner@cc.umanitoba.ca>
2013-03-15 21:35:10 -0500

    Poll submission and ViewPollDetail

---
+++ UVS/UVSViewPollDetail.h
@@ -0,0 +1,15 @@
+//
+//  UVSViewPollDetail.h
+//  UVS
+//
+//  Created by Richard Bruneau on 2013-03-15.
+//  Copyright (c) 2013 Richard Bruneau. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UVSViewPollDetail : UIViewController
+
+@property (nonatomic) BOOL *testBool;
+
+@end
---
+++ UVS/UVSViewPollDetail.m
@@ -0,0 +1,38 @@
+//
+//  UVSViewPollDetail.m
+//  UVS
+//
+//  Created by Richard Bruneau on 2013-03-15.
+//  Copyright (c) 2013 Richard Bruneau. All rights reserved.
+//
+
+#import "UVSViewPollDetail.h"
+
+@interface UVSViewPollDetail ()
+
+@end
+
+@implementation UVSViewPollDetail
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
