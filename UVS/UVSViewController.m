0139982afe27de0c7ba871ef3a7b0a77de8cef4c
Umbruner <umbruner@cc.umanitoba.ca>
2013-03-21 17:45:38 -0500

    Poll submission and poll display
    Poll feature is completed in this commit.

--- UVS/UVSViewController.m
+++ UVS/UVSViewController.m
@@ -2,6 +2,9 @@
 //  UVSViewController.m
 //  UVS
 //
+//  Created by Richard Bruneau on 2013-03-13.
+//  Copyright (c) 2013 Richard Bruneau. All rights reserved.
+//
 
 #import "UVSViewController.h"
 
--- UVS/UVSViewPollDetail.m
+++ UVS/UVSViewPollDetail.m
@@ -1,6 +1,7 @@
 //
 //  UVSViewPollDetail.m
 //  UVS
+
 //
 
 #import "UVSViewPollDetail.h"
@@ -12,6 +13,9 @@ NSMutableData *jsonData;
 NSMutableArray *choiceArray;
 NSMutableArray *cidArray;
 NSMutableArray *countArray;
+    
+UIView *buttonView;
+UIView *resultsView;
 
 }
 @end
@@ -37,8 +41,14 @@ NSMutableArray *countArray;
     [super viewDidLoad];
 	// Do any additional setup after loading the view.
     
-    pollDetailText.text = [NSString stringWithFormat:@"%d", (int) self.pollNum];
+    choiceArray = [[NSMutableArray alloc] init];
+    cidArray = [[NSMutableArray alloc] init];
+    countArray = [[NSMutableArray alloc] init];
+    
+    //pollDetailText.text = [NSString stringWithFormat:@"%d", (int) self.pollNum];
     pollTitleLabel.text = self.pollName;
+    
+    buttonView = [[UIView alloc] initWithFrame:CGRectMake(89, 157, 600, 600)];
     
     [self getPollByPid:self.pollNum];
     
@@ -55,7 +65,12 @@ NSMutableArray *countArray;
 //get the poll JSON object by pid
 - (void)getPollByPid:(int)pidInt
 {
-    NSLog(@"param - getPollByPid: %d", pidInt);
+    
+    [choiceArray removeAllObjects];
+    [cidArray removeAllObjects];
+    [countArray removeAllObjects];
+    
+        NSLog(@"param - getPollByPid: %d", pidInt);
     
     NSString *urlStr = [NSString stringWithFormat:@"http://ec2-54-235-121-23.compute-1.amazonaws.com:8274/polls/%d", self.pollNum];
     NSURL *url = [NSURL URLWithString:urlStr];
@@ -67,7 +82,7 @@ NSMutableArray *countArray;
     
     NSData *responseData = [NSURLConnection sendSynchronousRequest:request returningResponse:&response error:&err];
     
-    NSLog(@"Response: %@", responseData);
+        NSLog(@"Response: %@", responseData);
     
     jsonData = [[NSMutableData alloc]init];
     [jsonData setLength:0];
@@ -82,6 +97,7 @@ NSMutableArray *countArray;
     NSArray *choices = [poll objectForKey:@"choices"];
     
     for (NSDictionary *dict in choices) {
+        
         NSString *choiceTitle = [dict objectForKey:@"content"];
         NSInteger cid = [[dict objectForKey:@"id"] intValue];
         NSInteger chosenTimes = [[dict objectForKey:@"chosenTimes"] intValue];
@@ -97,13 +113,9 @@ NSMutableArray *countArray;
 //return array of choices
 - (void)generateVoteButtons{
     
-    UIView *buttonView = [[UIView alloc] initWithFrame:CGRectMake(89, 157, 600, 600)];
-    
     int size = [choiceArray count];
     
-    NSLog(@"count: %d", size);
-    
-    for (int i = 0; i < 5; i++) {
+    for (int i = 0; i < size; i++) {
         
         UIButton * button = [UIButton buttonWithType:UIButtonTypeRoundedRect];
         button.frame = CGRectMake(58, 75 * i, 200, 65);
@@ -112,7 +124,9 @@ NSMutableArray *countArray;
         
         [button setTitle:title forState:UIControlStateNormal];
         
-        [button addTarget:self action:@selector(buttonPressed:) forControlEvents:UIControlEventTouchUpInside];
+        [button setTag:[[cidArray objectAtIndex:i] intValue]];
+        
+        [button addTarget:self action:@selector(buttonTouchUpInside:) forControlEvents:UIControlEventTouchUpInside];
         
         [buttonView addSubview: button];
         
@@ -126,11 +140,13 @@ NSMutableArray *countArray;
 - (IBAction) buttonTouchUpInside:(id)sender {
     UIButton *buttonClicked = (UIButton *)sender;
     
-    //call submitPollVote
-    //get tag for button -send as choiceID
-    //send pidInt
+    buttonClicked.selected = YES;
     
-    NSLog(@"Pressed");
+    [self submitPollVote:self.pollNum cid:buttonClicked.tag];
+    
+    [self displayPollResults];
+
+    NSLog(@"Pressed - %d", buttonClicked.tag);
     
 }
 
@@ -140,7 +156,94 @@ NSMutableArray *countArray;
 {
     NSLog(@"param - submitPollVote: %d  %d", pidInt, choiceID);
     
+    //i.e: curl -X PUT -d "cid=94" http://ec2-54-235-121-23.compute-1.amazonaws.com:8274/polls/42/choices
     
+    //parameter set to "cid=y" (or whichever choice) sent to "PUT /polls/x/choices"
+    NSString *urlStr = [NSString stringWithFormat:@"http://ec2-54-235-121-23.compute-1.amazonaws.com:8274/polls/%d/choices", pidInt];
+    NSURL *url = [NSURL URLWithString:urlStr];
+    NSMutableURLRequest *request = [NSMutableURLRequest requestWithURL:url];
+    
+    //:8274/polls/x/choices - x = pid - param is cid=y, y being the choice id
+    NSString *dataStr = [NSString stringWithFormat:@"cid=%d", choiceID];
+    
+    //all other HTTP values set automatically
+    [request setHTTPMethod:@"PUT"];
+    [request setValue:@"application/x-www-form-urlencoded" forHTTPHeaderField:@"Content-Type"];
+    [request setHTTPBody:[dataStr dataUsingEncoding:NSASCIIStringEncoding allowLossyConversion:YES]];
+    
+    //response 
+    NSURLResponse *response;
+    NSError *err;
+    NSData *responseData = [NSURLConnection sendSynchronousRequest:request returningResponse:&response error:&err];
+    
+    
+    if (err == nil){
+        
+        pollDetailText.text = @"Vote submission successful.";
+        
+    }else{
+        
+        pollDetailText.text = @"Error. You have been nullified in the democratic process...";
+        
+    }
+    
+    
+    //Error and test output
+    
+    NSLog(@"Error(s): %@", err);
+    
+    NSLog(@"RESPONSE: %@", responseData);
+    
+}
+
+
+//display results using progress bars
+- (void)displayPollResults{
+    
+    [self getPollByPid:self.pollNum];
+    
+    [buttonView setUserInteractionEnabled:NO];
+    
+    resultsView = [[UIView alloc] initWithFrame:CGRectMake(89, 550, 600, 380)];
+    
+    int size = [countArray count];
+    
+        NSLog(@"SIZE: %d", size);
+    
+    NSNumber *arrTotal = [countArray valueForKeyPath:@"@sum.self"];
+    
+        NSLog(@"arrTOTAL: %@", arrTotal);
+    
+    int total = [arrTotal intValue];
+    
+    if (total > 0) {
+    
+        for (int i = 0; i < size; i++) {
+            
+            UIProgressView *progBar = [[UIProgressView alloc] init];
+            
+                NSLog(@"COUNT: %d", [[countArray objectAtIndex:i] intValue]);
+                NSLog(@"TOTAL: %d", total);
+            
+            progBar.progress = ( ((double) [[countArray objectAtIndex:i] intValue]) /((double) total));
+            
+                NSLog(@"PROG: %f",  ( ((double) [[countArray objectAtIndex:i] intValue]) /((double) total)) );
+            
+            progBar.frame = CGRectMake(58, 55 * i, 400, 60);
+
+            [resultsView addSubview:progBar];
+            
+        }
+        
+    }else{
+        
+        UITextView *note = [[UITextView alloc]init];
+        note.text = @"There are no results to display";
+        [resultsView addSubview:note];
+        
+    }
+    
+    [self.view addSubview:resultsView];
     
 }
 
