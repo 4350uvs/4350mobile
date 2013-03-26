8bafa71b70efe24373e7e74ca0befb016749ff6f
Richard <umbruner@gmail.com>
2013-03-26 17:55:32 -0500

    Update

--- UVSTests/UVSTests.m
+++ UVSTests/UVSTests.m
@@ -6,28 +6,39 @@
 //
 
 #import "UVSTests.h"
+#import "connectWithAppServer.h"
 
 @implementation UVSTests
+
 
 - (void)setUp
 {
     [super setUp];
-    
-    // Set-up code here.
 }
+
 
 - (void)tearDown
 {
-    // Tear-down code here.
-    
     [super tearDown];
 }
 
-- (void)testExample
+
+- (void)test_connection_works
 {
     
+    connectWithAppServer *connectToAPI = [connectWithAppServer alloc];
     
+    NSMutableArray *responseArray = [connectToAPI connectWithAppServerAtURL:[NSString stringWithFormat:@""]
+                                                                paramToSend:[NSString stringWithFormat:@""]
+                                                                methodToUse:@"GET"];
+    
+    STAssertNotNil(connectToAPI, @"Connection is null");
+    
+    NSURLResponse *response = [responseArray objectAtIndex:1];
+    
+    STAssertNotNil(response, @"Response is null");
     
 }
+
 
 @end
