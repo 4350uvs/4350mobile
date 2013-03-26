4f89282b484b4c762f832b0feb56284a1f78a7f0
Zhao Han <cx.chenghai+github@gmail.com>
2013-03-26 12:57:58 -0500

    Poll creation: Add testInitialEnabledChoiceFields test

--- UVSTests/UVSCreatePollTest.m
+++ UVSTests/UVSCreatePollTest.m
@@ -21,4 +21,16 @@
     STAssertNotNil(segmentedControl, @"the segmented control for poll creation is nil.");
 }
 
+- (void)testInitialEnabledChoiceFields {
+
+    UISegmentedControl *segmentedControl = uvsViewCreatePoll.choiceSeg;
+
+    [uvsViewCreatePoll performSelectorOnMainThread:@selector(choiceSegment:) withObject:segmentedControl waitUntilDone:YES];
+
+    NSMutableArray *pollChoices = [[NSMutableArray alloc]initWithObjects: uvsViewCreatePoll.pollChoice1, uvsViewCreatePoll.pollChoice2, uvsViewCreatePoll.pollChoice3, uvsViewCreatePoll.pollChoice4, uvsViewCreatePoll.pollChoice5, nil];
+
+    STAssertTrue([[pollChoices objectAtIndex:0] isEnabled], @"The first choice field should be enabled.");
+    STAssertTrue([[pollChoices objectAtIndex:1] isEnabled], @"The second choice field should be enabled.");
+}
+
 @end
